a5506e0bcdf415911ade1692fe0f25640c54ecee
colindurocher <lonepanda550@gmail.com>
2023-07-07 19:09:57 -0500

    everything totally works now

---
+++ scenes/World/Camera2D.gd
@@ -0,0 +1,10 @@
+extends Camera2D
+
+var spd = 4
+var radius_required_to_move = 400
+
+func _process(delta):
+	var mouse_position = get_global_mouse_position()
+	var mouse_delta = mouse_position - global_position
+	if (mouse_delta.length() >= radius_required_to_move):
+		position.x += lerp(position.x - global_position.x, mouse_delta.x, .5) * spd * delta
