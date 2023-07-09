e2e6bfafb872888f8f1fde4f68dc8082331549ca
colindurocher <lonepanda550@gmail.com>
2023-07-08 22:37:37 -0500

    things should be working

---
+++ globals/Score.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var day = 0
+var money = 0
+var citation = 0
---
+++ scenes/Between Day/BetweenDay.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("DaysCompleted").text = "Day " + str(Score.day) + " Completed"
+	get_node("HBoxContainer/Pay").text = "Current Bonus: " + str(Score.money)
+	get_node("HBoxContainer/Citations").text = "Current Citations: " + str(Score.citation)
+
+
+func _on_continue_pressed():
+	get_tree().change_scene_to_file("res://scenes/World/world.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
